809b1b18c6e956f6f280c73c0b0886efeef7f6b7
Peder Schulze <data@pederschulze.se>
2024-10-10 12:37:43 +0200

    Add project files.

---
+++ Globalization/Controllers/LanguageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Globalization.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public IActionResult ChangeCulture(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddMonths(6),
+                });
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
---
+++ Globalization/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddLocalization(opt =>
+{
+    opt.ResourcesPath = "Resources";
+});
+
+builder.Services.AddMvc()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services
+    .Configure<RequestLocalizationOptions>(opt =>
+    {
+        var supportedCultures = new List<CultureInfo>
+        {
+            new CultureInfo("en"),
+            new CultureInfo("sv"),
+            new CultureInfo("fi"),
+        };
+        opt.DefaultRequestCulture = new RequestCulture("en");
+        opt.SupportedCultures = supportedCultures;
+        opt.SupportedUICultures = supportedCultures;
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
